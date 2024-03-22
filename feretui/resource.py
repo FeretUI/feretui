7f1613967676405751e9854544adc151b0dae9b4
jssuzanne <js.suzanne@gmail.com>
2024-03-22 22:38:59 +0100

    cherry pick Recource class + ViewList class

---
+++ feretui/resource.py
@@ -0,0 +1,290 @@
+from wtforms import IntegerField
+from wtforms.validators import InputRequired
+import urllib
+from feretui.menus import ToolBarMenu
+from feretui.session import Session
+from feretui.request import Request
+from feretui.response import Response
+from feretui.view import ListView
+from typing import TYPE_CHECKING, Callable
+
+if TYPE_CHECKING:
+    from feretui.feretui import FeretUI
+
+
+class Resource:
+    _code: str = None  # filled by the decorator
+    _label: str = None  # filled by the decorator
+
+    class Form:
+        pk = IntegerField(validators=[InputRequired()])
+
+    _list_view: ListView = ListView()
+    # _new_view: NewView = NewView()
+    # _form_view: FormView = FormView()
+    # _edit_view: EditView = EditView()
+
+    default_view: str = 'list'
+
+    on_list_create_button_redirect_to: str = 'new'
+    on_list_do_click_on_row_redirect_to: str = 'form'
+
+    on_new_after_create_redirect_to: str = 'form'
+
+    on_form_create_button_redirect_to: str = 'new'
+    on_form_edit_button_redirect_to: str = 'edit'
+    on_form_has_delete_button: bool = True
+    on_form_return_button_redirect_to: str = 'list'
+
+    on_edit_after_update_redirect_to: str = 'form'
+    on_edit_cancel_button_redirect_to: str = 'form'
+
+    @classmethod
+    def get_view_url_from_options(cls, base_url, options: dict) -> str:
+        viewcode = options.get('view')
+        if viewcode is None:
+            options['view'] = cls.default_view
+
+        querystring = urllib.parse.urlencode(options, doseq=True)
+        return f'{base_url}/action/resource-render?{querystring}'
+
+    @classmethod
+    def menu(cls, menu_cls=ToolBarMenu, label=None, **kwargs):
+        kwargs = kwargs.copy()
+        if 'aside' in kwargs:
+            kwargs['aside_page'] = "resource"
+        else:
+            kwargs['page'] = "resource"
+
+        return menu_cls(
+            label or cls._label,
+            resource=cls._code,
+            **kwargs
+        )
+
+    @classmethod
+    def _router_render(cls, feret: "FeretUI", request: Request) -> Response:
+        viewcode = request.query['view'][0]
+        view = getattr(cls, f"_{viewcode}_view")
+        return Response(
+            view.render(cls, feret, request.session, request.query)
+        )
+
+    @classmethod
+    def _router_action(cls, feret: "FeretUI", request: Request) -> Response:
+        query = request.get_query_string_from_current_url()
+        viewcode = query['view'][0]
+        code = request.query['code'][0]
+        view = getattr(cls, f"_{viewcode}_view")
+        resource = cls()
+        ids = query.get('id')
+        if ids:
+            resource = cls(ids[0])
+
+        for actionset in view.actions:
+            for action in actionset.actions:
+                if action.code == code:
+                    res = getattr(resource, action.method)(feret, request)
+                    if res:
+                        if not isinstance(res, Response):
+                            res = Response(str(res))
+                    else:
+                        res = Response('')
+
+                    return res
+
+        return Response('')
+
+    @classmethod
+    def _router_row_action(
+        cls,
+        feret: "FeretUI",
+        request: Request
+    ) -> Response:
+        query = request.get_query_string_from_current_url()
+        viewcode = query['view'][0]
+        code = request.query['code'][0]
+        view = getattr(cls, f"_{viewcode}_view")
+        ids = request.query.get('id')
+        resource = cls(ids[0])
+
+        for field in view.fields:
+            if not isinstance(field, Actionset):
+                continue
+            for action in field.actions:
+                if action.code == code:
+                    res = getattr(resource, action.method)(feret, request)
+                    if res:
+                        if not isinstance(res, Response):
+                            res = Response(str(res))
+                    else:
+                        res = Response('')
+
+                    return res
+
+        return Response('')
+
+    @classmethod
+    def _router_pagination(
+        cls,
+        feret: "FeretUI",
+        request: Request
+    ) -> Response:
+        newqs = request.get_query_string_from_current_url().copy()
+        newqs.update(request.query)
+        viewcode = newqs['view'][0]
+        view = getattr(cls, f"_{viewcode}_view")
+        return Response(
+            view.render(feret, request.session, newqs, cls),
+            headers={
+                'HX-Push-Url': request.get_url_from_dict(newqs),
+            }
+        )
+
+    @classmethod
+    def _router_filter(
+        cls,
+        feret: "FeretUI",
+        request: Request
+    ) -> Response:
+        newqs = request.get_query_string_from_current_url().copy()
+        viewcode = newqs['view'][0]
+        view = getattr(cls, f"_{viewcode}_view")
+        mode = request.query.get('mode', ['add'])[0]
+        field_name = (
+            f"filter-{request.query['name'][0]}-{request.query['operator'][0]}"
+        )
+        newqs['offset'] = ['0']
+        filters = newqs.setdefault(field_name, [])
+        filters_ = filters.pop().split(',') if filters else []
+        value = request.query['value'][0]
+        if mode == 'add':
+            if value not in filters_:
+                filters_.append(value)
+        else:
+            filters_ = [x for x in filters_ if x != value]
+            if not filters_:
+                newqs.pop(field_name)
+
+        filters.append(','.join(filters_))
+        return Response(
+            view.render(feret, request.session, newqs, cls),
+            headers={
+                'HX-Push-Url': request.get_url_from_dict(newqs),
+            }
+        )
+
+    @classmethod
+    def _router_create(
+        cls,
+        feret: "FeretUI",
+        request: Request
+    ) -> Response:
+        try:
+            Schema = cls._new_view.get_pydantic_schema(feret, request, cls)
+            id = cls().create(**dict(Schema(**request.body)))
+            query = request.get_query_string_from_current_url()
+            query['view'] = [cls.on_new_after_create_redirect_to]
+            query['id'] = id
+            return Response(
+                '',
+                headers={
+                    'HX-Redirect': request.get_url_from_dict(query)
+                }
+            )
+        except ValidationError as e:
+            return Response(
+                feret.load_page_template(
+                    request.session,
+                    'feretui-page-pydantic-failed',
+                    errors=format_errors(e.errors()),
+                )
+            )
+        except Exception:
+            return Response(
+                feret.load_page_template(
+                    request.session,
+                    'feretui-page-error-500'
+                )
+            )
+
+    @classmethod
+    def _router_update(
+        cls,
+        feret: "FeretUI",
+        request: Request
+    ) -> Response:
+        try:
+            Schema = cls._edit_view.get_pydantic_schema(feret, request, cls)
+            ids = request.query['id']
+            cls(ids[0]).update(**dict(Schema(**request.body)))
+            query = request.get_query_string_from_current_url()
+            query['view'] = [cls.on_edit_after_update_redirect_to]
+            return Response(
+                '',
+                headers={
+                    'HX-Redirect': request.get_url_from_dict(query),
+                }
+            )
+        except ValidationError as e:
+            return Response(
+                feret.load_page_template(
+                    request.session,
+                    'feretui-page-pydantic-failed',
+                    errors=format_errors(e.errors()),
+                )
+            )
+        except Exception:
+            return Response(
+                feret.load_page_template(
+                    request.session,
+                    'feretui-page-error-500'
+                )
+            )
+
+    @classmethod
+    def _router_delete(
+        cls,
+        feret: "FeretUI",
+        request: Request
+    ) -> Response:
+        cls(request.query['id'][0]).delete()
+        return Response(
+            '',
+            headers={
+                'HX-Redirect': request.get_url_from_dict({
+                    'page': ['resource'],
+                    'resource': [cls._code],
+                    'view': ['list'],
+                }),
+            }
+        )
+
+    def __init__(self, pk):
+        self.pk = pk
+
+    @classmethod
+    def create(cls, form):
+        raise ResourceError('Must be overwriting')
+
+    @classmethod
+    def filtered_reads(
+        cls,
+        form_cls,
+        filters: list[tuple[str, str, str]],
+        offset: int,
+        limit: int,
+    ):
+        raise ResourceError('Must be overwriting')
+
+    def read(
+        self,
+        form_cls,
+    ):
+        raise ResourceError('Must be overwriting')
+
+    def update(self, form):
+        raise ResourceError('Must be overwriting')
+
+    def delete(self):
+        raise ResourceError('Must be overwriting')
---
+++ feretui/view.py
@@ -0,0 +1,131 @@
+import urllib
+from wtforms.fields.core import UnboundField
+from feretui.form import FeretUIForm
+
+
+class ViewError(Exception):
+    pass
+
+
+class View:
+
+    def __init__(
+        self,
+        label: str = None,
+    ):
+        self.Resource = None  # filled by resource
+        self.label = label
+
+    def render(
+        self,
+        feret,
+        session,
+        options: dict[str, list[str]],
+        ResourceCls,
+    ) -> str:
+        raise ViewError('render must be overwriting')
+
+    def get_label(self):
+        return self.label or 'List'
+
+    def get_transition_querystring(
+        self,
+        options: dict,
+        **kwargs: dict[str, str],
+    ):
+        options = options.copy()
+        for key, value in kwargs.items():
+            if value is None:
+                options.pop(key, None)
+            elif isinstance(value, list):
+                options[key] = value
+            else:
+                options[key] = [value]
+
+        return urllib.parse.urlencode(options, doseq=True)
+
+
+class ListView(View):
+    template_type: str = 'list'
+
+    def __init__(
+        self,
+        fields: list[str] = None,
+        # filters: list[Filter] = None,
+        limit: int = 15,
+        label: str = None,
+        # actions: list[Actionset] = None,
+    ):
+        View.__init__(self, label=label)
+        # ActionsMixinForView.__init__(self, actions=actions)
+        self.fields = fields
+        # self.filters = filters
+        self.limit = limit
+
+    def render(
+        self,
+        feret,
+        session,
+        options: dict,
+        ResourceCls,
+    ) -> str:
+        properties = {}
+
+        if self.fields:
+            for attr in dir(ResourceCls.Form):
+                field = getattr(ResourceCls.Form, attr)
+                if isinstance(field, UnboundField):
+                    if attr not in self.fields:
+                        properties[attr] = None
+        else:
+            self.fields = [
+                attr
+                for attr in dir(ResourceCls.Form)
+                if isinstance(getattr(ResourceCls.Form, attr), UnboundField)
+            ]
+
+        form_cls = type(
+            f'{ResourceCls._code}__list',
+            (ResourceCls.Form, FeretUIForm),
+            properties,
+        )
+
+        offset = options.get('offset', 0)
+        if isinstance(offset, list):
+            offset = offset[0]
+
+        offset = int(offset)
+        dataset = ResourceCls.filtered_reads(
+            form_cls,
+            None,  # filters,
+            offset,
+            self.limit,
+        )
+        paginations = range(0, dataset['total'], self.limit)
+
+        create_view_qs = self.get_transition_querystring(
+            options,
+            view=ResourceCls.on_list_create_button_redirect_to,
+        )
+
+        open_view_qs = self.get_transition_querystring(
+            options,
+            id=None,
+            view=ResourceCls.on_list_do_click_on_row_redirect_to,
+        )
+
+        return feret.render_template(
+            session,
+            'feretui-page-resource-list',
+            label=self.get_label(),
+            Resource=ResourceCls,
+            fields=self.fields,
+            form=form_cls(),
+            # filters=filters,
+            offset=offset,
+            limit=self.limit,
+            paginations=paginations,
+            dataset=dataset,
+            create_view_qs=create_view_qs,
+            open_view_qs=open_view_qs,
+        )
